ed6b50466e41514eb6c5255dbd96fa6f1895f869
Ilya <sckremyulock@gmail.com>
2022-12-15 01:19:08 +0500

    fixed cli

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from http_client import HttpClient, write_response_to_file
+from http_client import HttpClient
 from sockets import HttpSocket, HttpsSocket
 import click
 
@@ -6,7 +6,7 @@ import click
 @click.command()
 @click.argument('method', nargs=1, default='GET')
 @click.argument('url', nargs=1)
-@click.option('--headers', '-H', nargs=1, type=str)
+@click.option('--headers', '-H', type=str)
 @click.option('--data', '-d', help='Data to send')
 @click.option('--output', '-o', help='Output file', default=None)
 @click.option('--user', '-u', help='Username')
@@ -16,14 +16,14 @@ import click
 @click.option('--user_agent', '-A', help='User-Agent')
 @click.option('--cookie', '-c', help='Cookie')
 @click.option('--websocket', '-w', is_flag=True, help='Use WebSocket')
-def main(method, url, headers, data, output, user, password, https, timeout, user_agent, cookie, websocket): # noqa: E501
+def main(method, url, headers, data, output, user, password, https, timeout, user_agent, cookie, websocket):  # noqa: E501
     if '/' in url:
         host, url = url.split('/', 1)
         url = '/' + url
     else:
         host = url
         url = '/'
-    headers = {'headers': headers}
+    headers = {'headers': headers} if headers else {}
     if user and password:
         user = f'{user}:{password}'
         headers['user'] = user
@@ -37,8 +37,10 @@ def main(method, url, headers, data, output, user, password, https, timeout, use
     sock = (HttpsSocket(host, timeout=timeout) if https
             else HttpSocket(host, timeout=timeout))
 
-    client = HttpClient(url, sock)
-    client.get_response(method, url, **headers)
+    client = HttpClient(host, sock)
+    for chunk in client.get_response(method, url, **headers):
+        print(chunk)
 
 
-
+if __name__ == '__main__':
+    main()
